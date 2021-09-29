31871d0de3447eca4c8a5390a757840ce797847e
Anderson <laventurepsu1@gmail.com>
2021-09-29 19:35:06 -0400

    chore: added comment on event listener

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 let color = '#3a41a7';
 
+//set event listener on chrome extension
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to blue', `color: ${color}`);
